6cc3f26be7050c35b509ff9cf322906456a79f96
Sebastián Larrauri <slarrauri@gmail.com>
2024-02-10 15:58:23 -0300

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://apeiron.bio'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Apeiron.bio </span>
       </a>
     </span>
   );
